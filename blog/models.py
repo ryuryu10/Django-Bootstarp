e36cb473c829f681edbf537b2f2f21b0380b86ac
Seungwoo Yoo <ryuryu10@naver.com>
2021-11-25 00:56:27 +0900

    dev

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.db.models.base import Model
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30) #CharFiedl클래스의 최대길이는 30
+    content = models.TextField() #TextField / 문자열의 길이 제한이 없음
+
+    created_at = models.DateTimeField() #월,일,시,분,초까지 기록할수 있는 필드
