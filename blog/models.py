b1ae9183af0c0db10eb524d31998c9b7416406f6
Seungwoo Yoo <ryuryu10@naver.com>
2021-11-25 01:11:28 +0900

    dev

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,8 @@ class Post(models.Model):
     title = models.CharField(max_length=30) #CharFiedl클래스의 최대길이는 30
     content = models.TextField() #TextField / 문자열의 길이 제한이 없음
 
-    created_at = models.DateTimeField() #월,일,시,분,초까지 기록할수 있는 필드
+    created_at = models.DateTimeField(auto_now_add=True) #월,일,시,분,초까지 기록할수 있는 필드
+    updateed_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'[{self.pk}]{self.title}' #pk는 각 레코드에 대한 고유값이다
